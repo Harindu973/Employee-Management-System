33a25bc60c5a25d11ee9800e90d9c2bb72220f9c
kokum007 <dakokum@gmail.com>
2019-08-06 16:35:54 +0530

    email update

---
+++ Employee Management Software/Employee Management System/Employee Management System/email.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace Employee_Management_System
+{
+    public class Email
+    {
+        public void email()
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("181plymouthsoc@gmail.com");
+                mail.To.Add("dakokum@gmail.com");
+
+                mail.Subject = "Attendence sheet";
+
+                //body
+
+                mail.Body = "body";
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("181plymouthsoc@gmail.com", "123456789soc");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+
+                //MessageBox.Show("mail Send");
+            }
+            catch (Exception)
+            {
+               // MessageBox.Show(ex.ToString());
+            }
+
+
+        }
+        
+    }
+}
---
+++ Employee Management Software/Employee Management System/Employee Management System/testemail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Employee_Management_System
+{
+    public partial class testemail : Form
+    {
+        public testemail()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Email c1 = new Email();
+            c1.email();
+
+           
+            
+        }
+    }
+}
